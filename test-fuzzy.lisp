5b1ed2c94b755f22d58054c8bbe7945fe4c990ec
vindarel <vindarel@mailz.org>
2020-09-07 12:06:57 +0200

    tests: try Parachute
    output:
    
    TESTS> (test 'FUZZY-MATCH)
    
            ？ FUZZY-MATCH.TESTS::FUZZY-MATCH
      0.000 ✔   (is #'equal (first (fuzzy-match "hel" '("help-mode" "help" "foo-help" "help-foo-bar"))) "help")
      0.000 ✔   (is #'equal (first (fuzzy-match "hel" *suggestions*)) "HELP")
      0.000 ✔   (is #'equal (first (fuzzy-match "swit buf" '("about" "switch-buffer-" "switch-buffer" "delete-buffer"))) "switch-buffer")
      0.000 ✔   (is #'equal (first (fuzzy-match "swit buf" *suggestions*)) "SWITCH-BUFFER")
      0.000 ✔   (is #'equal (first (fuzzy-match "buf swit" '("about" "switch-buffer" "delete-buffer"))) "switch-buffer")
      0.000 ✔   (is #'equal (first (fuzzy-match "de" '("some-mode" "delete-foo"))) "delete-foo")
      0.000 ✔   (is #'equal (first (fuzzy-match "foobar" '("foo-dash-bar" "foo-bar"))) "foo-bar")
      0.000 ✔   (is #'equal (first (fuzzy-match "sbf" *suggestions*)) "SWITCH-BUFFER")
      0.000 ✔   (is #'equal (first (fuzzy-match "FOO" '("foo-dash-bar" "FOO-BAR"))) "FOO-BAR")
      0.000 ✔   (is #'equal (first (fuzzy-match "[" '("test1" "http://[1:0:0:2::3:0.]/" "test2"))) "http://[1:0:0:2::3:0.]/")
      0.070 ✔ FUZZY-MATCH.TESTS::FUZZY-MATCH
    
    ;; Summary:
    Passed:    10
    Failed:     0
    Skipped:    0
    
    cons:
    
    - the report doesn't use our string explanations
    - no setup/teardown (unused here though)

--- test-fuzzy.lisp
+++ test-fuzzy.lisp
@@ -1,12 +1,12 @@
 (defpackage :fuzzy-match.tests
   (:use :common-lisp
-        :prove)
+        :parachute)
   (:import-from :fuzzy-match
                 :fuzzy-match))
 
 (in-package :fuzzy-match.tests)
 
-(plan nil)
+;; (plan nil)
 
 
 (defparameter *suggestions* '(
@@ -25,52 +25,52 @@
                               )
   "Existing commands.")
 
-(subtest "Fuzzy match"
-  (is (first (fuzzy-match "hel"
+(define-test fuzzy-match
+  (is #'equal (first (fuzzy-match "hel"
                           '("help-mode" "help" "foo-help" "help-foo-bar")))
       "help")
-  (is (first (fuzzy-match "hel"
+  (is #'equal (first (fuzzy-match "hel"
                           *suggestions*))
       "HELP"
       "match 'help' with real suggestions list")
-  (is (first (fuzzy-match "swit buf"
+  (is #'equal (first (fuzzy-match "swit buf"
                           '("about" "switch-buffer-" "switch-buffer"
                             "delete-buffer")))
       "switch-buffer"
       "match 'swit buf' (small list)")
-  (is (first (fuzzy-match "swit buf"
+  (is #'equal (first (fuzzy-match "swit buf"
                           *suggestions*))
       "SWITCH-BUFFER"
       "match 'swit buf' with real suggestions list")
 
-  (is (first (fuzzy-match "buf swit"
+  (is #'equal (first (fuzzy-match "buf swit"
                           '("about" "switch-buffer" "delete-buffer")))
       "switch-buffer"
       "reverse match 'buf swit' (small list)")
   ;; TODO: Fix reverse fuzzy matching.
-  ;; (is (first (fuzzy-match "buf swit"
+  ;; (is #'equal (first (fuzzy-match "buf swit"
   ;;                               *suggestions*))
   ;;     "SWITCH-BUFFER"
   ;;     "reverse match 'buf swit' with real suggestions list")
 
-  (is (first (fuzzy-match "de"
+  (is #'equal (first (fuzzy-match "de"
                           '("some-mode" "delete-foo")))
       "delete-foo"
       "suggestions beginning with the first word appear first")
 
-  (is (first (fuzzy-match "foobar"
+  (is #'equal (first (fuzzy-match "foobar"
                           '("foo-dash-bar" "foo-bar")))
       "foo-bar"
       "search without a space. All characters count (small list).")
-  (is (first (fuzzy-match "sbf"
+  (is #'equal (first (fuzzy-match "sbf"
                           *suggestions*))
       "SWITCH-BUFFER"
       "search without a space. All characters count, real list.")
-  (is (first (fuzzy-match "FOO"
+  (is #'equal (first (fuzzy-match "FOO"
                           '("foo-dash-bar" "FOO-BAR")))
       "FOO-BAR"
       "input is uppercase (small list).")
-  (is (first (fuzzy-match "[" '("test1"
+  (is #'equal (first (fuzzy-match "[" '("test1"
                                 "http://[1:0:0:2::3:0.]/"
                                 "test2")))
       "http://[1:0:0:2::3:0.]/"
